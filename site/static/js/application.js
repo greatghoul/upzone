5c813a09d4e0440890bce980b090fe7e03d031da
greatghoul <greatghoul@gmail.com>
2015-02-06 01:09:24 +0800

    通过 data url 加载图片预览

--- site/static/js/application.js
+++ site/static/js/application.js
@@ -20,18 +20,56 @@ $(function() {
     });
   }
 
-  $('body').on('paste', function(event) {
-      var items = event.originalEvent.clipboardData.items;
+  function getPastedImage(event, callback) {
+    var items = event.originalEvent.clipboardData.items;
 
-      for (var i = 0; i < items.length; i++) {
-        var item = items[i];
+    for (var i = 0; i < items.length; i++) {
+      var item = items[i];
 
-        if (/image\/.*/i.test(item.type)) {
-          var file = item.getAsFile();
-          file.name = item.type.replace(/image\//i, 'paste.');
-          uploadFile(file);
-          break;
-        }
+      if (/image\/.*/i.test(item.type)) {
+        var file = item.getAsFile();
+        file.name = item.type.replace(/image\//i, 'paste.');
+        return callback(file);
       }
+    }
+
+    return null;
+  }
+
+  function previewImage(image) {
+    $('.container.active').removeClass('active');
+    $('.container-preview').addClass('active');
+
+    var $container = $('.container-preview');
+    var reader  = new FileReader();
+
+    reader.readAsDataURL(image);
+    reader.onload = function (event) {
+      var imageObj = new Image();
+      
+      var url = event.target.result;
+
+      imageObj.onload = function() {
+        var width  = $container.innerWidth();
+        var height = $container.innerHeight();
+
+        if (this.width > width || this.height > height) {
+          $container.css('background-size', 'contain');
+        } else {
+          $container.css('background-size', 'auto');
+        }
+
+        $container.css('background-image', 'url(' + url + ')');
+      };
+
+      imageObj.src = url;
+    };
+  }
+
+  $('body').on('paste', function(event) {
+    getPastedImage(event, function(image) {
+      previewImage(image);
+      // uploadFile(file);
+    });
   });
 });
