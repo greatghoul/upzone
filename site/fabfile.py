fb475b2897facbb225a328bea23458893423964c
greatghoul <greatghoul@gmail.com>
2016-05-16 02:19:07 +0800

    Change structure

--- site/fabfile.py
+++
@@ -1,7 +0,0 @@
-from fabric.decorators import task
-from main import app
-
-@task
-def server():
-    """Start development server"""
-    app.run(host="0.0.0.0", port=8080)
---
+++ upzone/config.py
@@ -0,0 +1,6 @@
+# Debug is disabled by default.
+DEBUG = True
+SECRET_KEY = 'suckmycock!'
+
+UPYUN_BUCKET   = 'greatghoul'
+UPYUN_DOMAIN   = 'greatghoul.b0.upaiyun.com'
